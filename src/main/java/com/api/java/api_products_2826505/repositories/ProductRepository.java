621fc33e3adad9b8f3c443f9f4937e4d796a6744
Your Name <you@example.com>
2024-07-24 17:45:06 -0500

    Servicion Creado

--- src/main/java/com/api/java/api_products_2826505/repositories/ProductRepository.java
+++ src/main/java/com/api/java/api_products_2826505/repositories/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.api.java.api_products_2826505.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 //Repositorio por cada entidad
+@Repository
 public interface ProductRepository<Product> extends CrudRepository<Product, Long>
 {
     
